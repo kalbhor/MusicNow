adc0e0256c1e37503ce012c0f98e4fba90f7d6dd
LakshayKalbhor <iilakshayii@gmail.com>
2016-08-27 02:03:22 +0530

    Handled non ascii characters

--- Music-Downloader.py
+++ Music-Downloader.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 import youtube_dl
 
 
+
+
 from collections import OrderedDict
 import os
 from sys import argv
@@ -34,8 +36,7 @@ from mutagen.id3 import ID3, APIC, error
 
 script,mode = argv
 
-def get_soup(url,header):
-    return BeautifulSoup(urllib2.urlopen(urllib2.Request(url,headers=header)),"html.parser")
+
 
 
 def prompt(url): #Definition to prompt for song number from list of songs
@@ -43,7 +44,9 @@ def prompt(url): #Definition to prompt for song number from list of songs
 	link = url.values()[x]
 	title = url.keys()[x]
 	os.system('clear')
-	print 'Download Song: %s  Y/N?' % title
+	print 'Download Song: ',
+	print title,
+	print 'Y/N?'
 	x = raw_input('>')
 	if x == 'Y' or x == 'y':
 		pass
@@ -62,12 +65,12 @@ def get_url(name):	#Method to get URL of Music Video from YouTube
 	urls_list = OrderedDict() #Create ordered Dictionary
 	num = 0			   #List of songs index
 	print '\n'
-	array = list(str(name)) 
-	for i in range(0,len(str(name))):
+	array = list(name)
+	for i in range(0,len(name)):
 		if array[i] ==' ':
 			array[i] = '+'
-	name = ''.join(array).encode('utf-8')
-	name = 'https://www.youtube.com/results?search_query=' + str(name)
+	name = ''.join(array)
+	name = 'https://www.youtube.com/results?search_query=' + name
 	html = requests.get(name)
 	soup = BeautifulSoup(html.text,'html.parser')
 
@@ -77,27 +80,30 @@ def get_url(name):	#Method to get URL of Music Video from YouTube
 	YT_Class = 'yt-uix-sessionlink yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2       spf-link '	#YouTube Class holding video
 
 	for i in soup.findAll('a',{'class' : YT_Class}): #In all Youtube Search Results
-		link = 'https://www.youtube.com' + str(i.get('href')).encode('utf-8')
+		link = 'https://www.youtube.com' + (i.get('href'))
 		link_title = (i.get('title')).encode('utf-8')
 		urls_list.update({link_title:link}) #Adds title and song url to dictionary
 
 		if mode == 'S': #Display list for single song mode
-			try:
-				print '['+str(num)+'] ' + link_title #Prints list
-			except UnicodeDecodeError: 
-				pass
+			#try:
+			print '['+str(num)+'] ', #Prints list
+			print link_title
+			#except UnicodeDecodeError: 
+			#	pass
 			num = num + 1
 
 		elif mode == 'M': #For multiple song mode, return the first result
 			return (urls_list.values()[0], urls_list.keys()[0])
 
 	title,url = prompt(urls_list) #Gets the demanded song title and url (only for single song mode)
+
 	return (url,title) #Returns Name of Song and URL of Music Video
 
 
 
 def download(url,title):
-	initial_title = title+'-'+url[32:]+'.mp3'
+	title = title.decode('utf-8')
+	initial_title = (title+'-'+url[32:]+'.mp3').encode('utf-8')
 	print initial_title
 	ydl_opts = {
 		'format': 'bestaudio/best',
@@ -109,7 +115,7 @@ def download(url,title):
 	}
 
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		ydl.download([str(url)]) #Downloads audio using youtube-dl
+		ydl.download([(url)]) #Downloads audio using youtube-dl
 
 	try:	
 		os.rename(initial_title,title+'.mp3') #Renames file to song title
@@ -123,15 +129,22 @@ def download(url,title):
 
 '''Album Art Fetching and Adding '''
 
+def get_soup(url,header):
+    return BeautifulSoup(urllib2.urlopen(urllib2.Request(url,headers=header)),"html.parser")
 
 def get_albumart(query): 
 	print "\nFetching Album Art.."
+	query = query.decode('utf-8')
+	query = query + " Album Art"
 	query = query.split()
 	query ='+'.join(query)
-	url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+	
+	url=("https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch").encode('utf-8')
+	print url
 
 	header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
 	}
+	url = url.decode('ascii','ignore')
 	soup = get_soup(url,header)
 
 
@@ -181,38 +194,32 @@ if mode == 'S' or mode =='s':
 	song_name = raw_input('Enter Song Name/Keywords : ') #Song Name as input or Keywords of Song
 	song_YT_URL,title = get_url(song_name) #Gets YT url
 
-
 	download(song_YT_URL,title) #Saves as .mp3 file
 
 	image = get_albumart(title) #Gets album art
-	add_albumart(image,title+'.mp3') #Adds album art to song
+	title = title.decode('utf-8')
+	title = title + '.mp3'
+	title = title.encode('utf-8')
+	add_albumart(image,title) #Adds album art to song
 
 elif mode == 'M' or mode == 'm':
 	file = raw_input('Enter file location > ') 
 
-	with open(str(file)) as f:
+	with open(file) as f:
 		content = f.readlines() #stores each song line by line
 	
 	for song_names in content: #iterates over each song name
-
+		song_names = song_names.decode('utf-8')
 		song_YT_URL,title = get_url(song_names) #Gets YT url
-		title = unicode(title,"utf-8")
+		
 
 		download(song_YT_URL,title) #Downloads song
 
-		image = get_albumart(title+' Album Art') #Gets album art
-		add_albumart(image,title+'.mp3') #Adds album art
+		image = get_albumart(title) #Gets album art
+		title = title.decode('utf-8')
+		title = title + '.mp3'
+		title = title.encode('utf-8')
+		add_albumart(image,title) #Adds album art
 
 else:
 	print('Error. Invalid mode.')
-
-
-
-
-
-
-
-
-
-
-
