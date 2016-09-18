fae22a88ee59bf6e885118717e2ebf7439fab945
LakshayKalbhor <iilakshayii@gmail.com>
2016-09-18 22:04:14 +0530

    Project Added to PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='musicdownload',
+setup(name='download-music',
       version='0.1',
       description='Lets you download music with album art and details',
       url='https://github.com/lakshaykalbhor/Music-Downloader',
       author='Lakshay Kalbhor',
       author_email='lakshaykalbhor@gmail.com',
       license='MIT',
-      packages=['musicdownload'],
-      scripts=["bin/musicdownload"],
+      packages=['download-music'],
+      scripts=["bin/download-music"],
       install_requires=[
           'youtube-dl',
           'bs4',
